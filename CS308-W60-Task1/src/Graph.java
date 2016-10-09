c9ffb9e5d936e15477d04b54755281459e0374d7
Stuart Baker <mjb14170@strath.ac.uk>
2016-10-09 14:49:38 +0100

    search and find path functions refactored

--- CS308-W60-Task1/src/Graph.java
+++ CS308-W60-Task1/src/Graph.java
@@ -69,66 +69,43 @@ public class Graph implements MultiGraphADT {
         graph.put(id, new Station(id, name));
     }
 
-    /*
-     * Don't think this method is needed anymore
-     * 
     public ArrayList<Integer> successors(int node) {
         ArrayList<Integer> successorNodes = new ArrayList<>();
-        for (int i = 0; i < nEdges(); i++)
-            if (lineList.get(i).getIn() == node)
-                successorNodes.add(lineList.get(i).getOut());
+        Iterator<IEdge> iter = nodeEdges.get(node).iterator();
+    	while (iter.hasNext()) {
+    		successorNodes.add(iter.next().getOut());
+    	}
         return successorNodes;
     }
-    */
+    
 
     @Override
     public void addNode(INode n) {
     }
 
-
-
-    /*Don't think this method is needed either
-     * 
-    private boolean isEdge(int n1, int n2) { // needed?
-        int i = 0;
-        while (i < nEdges()) {
-            if ((lineList.get(i).getIn() == n1) && (lineList.get(i).getOut() == n2)
-                    || (lineList.get(i).getIn() == n2) && (lineList.get(i).getOut() == n1))
-                return true;
-            else {
-                i++;
-            }
-        }
-        return false;
-    }
-    */
-
     @Override
     public ArrayList<INode> search(int start, int finish) {
 
-        int out = 0;
-
         ArrayList<Integer> visited = new ArrayList<>();
         Queue<Integer> q = new LinkedList<>();
+        ArrayList<Integer> outNodes = new ArrayList<>();
 
         q.add(start);
         while (!q.isEmpty()) {
         	int m = q.remove();
         	visited.add(m);
-        	Iterator<IEdge> iter = nodeEdges.get(m).iterator();
-        	while (iter.hasNext()) {
-        		out = iter.next().getOut();
-        	    if(out == finish){
+        	outNodes = successors(m);
+        	for(int out: outNodes){
+        		if(out == finish){
         	    	visited.add(out);
-        	    	return findPath(visited);
+        	    	break;
         	    }
         	    if(!visited.contains(out) && out != 0 && !q.contains(out)){
             	    q.add(out);	
         	    }
-        	}            
+        	}         
         }
-        //we should never get to here as there should always be a path
-        return null;
+        return findPath(visited);
     }
     
     
@@ -139,14 +116,10 @@ public class Graph implements MultiGraphADT {
         path.add(graph.get(nodes.get(nodes.size() - 1)));
 
         int index = nodes.size() - 1;
-        ArrayList<Integer> connectedNodes;
+        ArrayList<Integer> connectedNodes = new ArrayList<Integer>();;
 
         while (index != 0) {
-        	connectedNodes = new ArrayList<Integer>();
-        	Iterator<IEdge> iter = nodeEdges.get(nodes.get(index)).iterator();
-        	while (iter.hasNext()) {
-        		connectedNodes.add(iter.next().getOut());
-        	}
+        	connectedNodes = successors(nodes.get(index));
             for (int i = 0; i < index; i++) {
             	if(connectedNodes.contains(nodes.get(i))){
             		path.add(graph.get(nodes.get(i)));
